8dc573be5a942777fb2796a4ba714be79c04d11d
Julio <juliorodriguezramirez@gmail.com>
2023-11-28 12:00:42 +0100

    Migrate templates and components

---
+++ src/app/components/header/header.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { HeaderComponent } from './header.componet';
+
+describe('HeaderComponent', () => {
+    let component: HeaderComponent;
+    let fixture: ComponentFixture<HeaderComponent>;
+
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            imports: [RouterTestingModule]
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(HeaderComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,119 +1,57 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { filter, Observable, Subject, takeUntil } from 'rxjs';
-// import { Router } from '@angular/router';
-
-import { AuthService } from '../../auth/auth.service';
-import { ModalDialogComponent } from '../../components/modal-dialog/modal-dialog.component';
-import { User } from '../../../models/user.model';
-import { UserDoc } from '../../../models/ddbb.model';
-import { UserService } from '../../services/user.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-	selector: 'app-dashboard',
-	templateUrl: './dashboard.component.html',
-	styleUrls: ['./dashboard.component.scss'],
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit, OnDestroy {
-	public userLogued$!: Observable<User | null>;
-	private destroyed$ = new Subject<void>();
-	public name: string = '';
-	public city: string = '';
-	public coordinates: { latitude: number; longitude: number } | undefined;
-	public users: any[] = [];
-	public showAllUsers = false;
+export class DashboardComponent implements OnInit {
+    public alerts: Array<any> = [];
+    public sliders: Array<any> = [];
 
-	constructor(
-		private userService: UserService,
-		private authService: AuthService,
-		private router: Router,
-		private modalService: NgbModal
-	) {}
+    constructor() {
+        this.sliders.push(
+            {
+                imagePath: 'assets/images/slider1.jpg',
+                label: 'First slide label',
+                text: 'Nulla vitae elit libero, a pharetra augue mollis interdum.'
+            },
+            {
+                imagePath: 'assets/images/slider2.jpg',
+                label: 'Second slide label',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+            },
+            {
+                imagePath: 'assets/images/slider3.jpg',
+                label: 'Third slide label',
+                text: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
+            }
+        );
 
-	ngOnInit() {
-		// consume for Subject
-		this.userLogued$ = this.authService.user$.pipe(
-			filter(user => !!user),
-			takeUntil(this.destroyed$)
-		);
+        this.alerts.push(
+            {
+                id: 1,
+                type: 'success',
+                message: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptates est animi quibusdam praesentium quam, et perspiciatis,
+                consectetur velit culpa molestias dignissimos
+                voluptatum veritatis quod aliquam! Rerum placeat necessitatibus, vitae dolorum`
+            },
+            {
+                id: 2,
+                type: 'warning',
+                message: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptates est animi quibusdam praesentium quam, et perspiciatis,
+                consectetur velit culpa molestias dignissimos
+                voluptatum veritatis quod aliquam! Rerum placeat necessitatibus, vitae dolorum`
+            }
+        );
+    }
 
-		this.userLogued$.subscribe(user => {
-			if (user && user.address) {
-				this.name = user.name;
-				this.city = user.address.city;
-			}
-		});
+    ngOnInit() {}
 
-		this._loadUsersData();
-	}
-
-	ngOnDestroy() {
-		this.destroyed$.next();
-		this.destroyed$.complete();
-	}
-
-	private _loadUsersData() {
-		this.userService
-			.getUsers()
-			.pipe(takeUntil(this.destroyed$))
-			.subscribe((doc: UserDoc[]) => {
-				let order = 0;
-				this.users = doc.map((user: UserDoc) => {
-					++order;
-					return {
-						...user,
-						order,
-						addressRe: `${user.address.street}, ${user.address.city}`,
-					};
-				});
-			});
-	}
-
-	toggleShowAllUsers() {
-		this.showAllUsers = !this.showAllUsers;
-	}
-
-	getUsersToShow() {
-		if (this.showAllUsers) {
-			return this.users;
-		} else {
-			return this.users.slice(0, 7);
-		}
-	}
-
-	onSignOut() {
-		// this.authService.logout();
-	}
-
-	onSettingsUser() {
-		const modalRef = this.modalService.open(ModalDialogComponent, {
-			centered: true,
-			backdrop: 'static',
-		});
-		(modalRef.componentInstance as ModalDialogComponent).udpateCredentials = true;
-
-		modalRef.result.then(
-			() => {
-				// this.authService.updateCredentials(result.email, result.password).then(
-				//   () => {
-				//     this.router.navigate(['login']);
-				//   },
-				//   (err) => console.error(err)
-				// );
-				// this.authService.logout();
-			},
-			reason => {
-				console.info(`Dismissed with: ${reason}`);
-			}
-		);
-	}
-
-	onInfoUser(selected: UserDoc) {
-		const modalRef = this.modalService.open(ModalDialogComponent, {
-			centered: true,
-			backdrop: 'static',
-		});
-		(modalRef.componentInstance as ModalDialogComponent).user = selected;
-	}
+    public closeAlert(alert: any) {
+        const index: number = this.alerts.indexOf(alert);
+        this.alerts.splice(index, 1);
+    }
 }
