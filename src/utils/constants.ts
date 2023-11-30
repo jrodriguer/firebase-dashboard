f6c03c03d9f727dd16cdd60e04f5675683286bdd
Julio <juliorodriguezramirez@gmail.com>
2023-11-30 19:00:25 +0100

    Fix endpoint uri

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,20 +1,20 @@
 import { HttpHeaders } from '@angular/common/http';
 import { environment } from '../environments/environment';
 
-const BASE_URI_DES = `https://backend-des.wenea.site/api`;
-const BASE_URI_PRE = `https://backend-pre.wenea.site/api`;
-const BASE_URI_PRO = `https://backend-pro.wenea.site/api`;
-const BASE_URI_DEHESA = `https://backend-dehesa.wenea.site/api`;
+const BASE_URI_DES: string = `https://backend-des.wenea.site/api`;
+const BASE_URI_PRE: string = `https://backend-pre.wenea.site/api`;
+const BASE_URI_PRO: string = `https://backend-pro.wenea.site/api`;
+const BASE_URI_DEHESA: string = `https://backend-dehesa.wenea.site/api`;
 
-const WENEA_API_VERSION = 'v7';
-const WENEA_VERSION = environment.version;
-const BASE_URI: string = BASE_URI_DEHESA;
-const USER_ENDPOINT = `${BASE_URI}/${WENEA_API_VERSION}/user`;
+const WENEA_API_VERSION: string = 'v7';
+export const WENEA_VERSION: string = environment.version;
+const BASE_URI: string = BASE_URI_PRO;
+const USER_ENDPOINT: string = `${BASE_URI}/${WENEA_API_VERSION}/user`;
 
-export const WENEA_USER_LOGIN = `${USER_ENDPOINT}/login/`;
-export const WENEA_USER_PROFILE = `${USER_ENDPOINT}/info/`;
+export const WENEA_USER_LOGIN: string = `${USER_ENDPOINT}/login/`;
+export const WENEA_USER_PROFILE: string = `${USER_ENDPOINT}/profile/`;
 
-export const BASE_REST_HEADER = new HttpHeaders({
+export const BASE_REST_HEADER: HttpHeaders = new HttpHeaders({
 	'Content-Type': 'application/json',
 	'X-App-Version': WENEA_VERSION,
 });
