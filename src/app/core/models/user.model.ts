032cbcd3df3f68c8390db64370e99b516768bf87
Julio <juliorodriguezramirez@gmail.com>
2023-11-30 22:55:51 +0100

    Remove class

--- src/app/core/models/user.model.ts
+++ src/app/core/models/user.model.ts
@@ -5,9 +5,85 @@ export interface Address {
   country: string;
 }
 
+export interface ChargingPoint {
+  code: number;
+  desc: string;
+  name: string;
+  standard_name: string;
+  image_url: string;
+}
+
+export interface ChargingCurve {
+  power: number;
+  percentage: number;
+}
+
+export interface Charger {
+  ports: ChargingPoint[];
+  max_power: number;
+  charging_curve: ChargingCurve[];
+  is_default_charging_curve: boolean;
+}
+
+export interface EnergyConsumption {
+  average_consumption: number;
+}
+
+export interface VehicleModel {
+  id: string;
+  brand_id: string;
+  model: string;
+  release_year: number;
+  variant: string;
+  usable_battery_size: string;
+  energy_consumption: EnergyConsumption;
+  vehicle_type: string;
+  type: string;
+  ac_charger: Charger;
+  dc_charger: Charger;
+  image_url: string;
+  has_autocharge: boolean;
+}
+
+export interface User {
+  id: number;
+  mail: string;
+  id_tag: string;
+  photo: string;
+  groups: Group[];
+  vehicles: Vehicle[];
+  feature_flags: FeatureFlags;
+  status: string;
+  location: Address;
+  privateInfo: PrivateInfo;
+  paymentInfo: any[]; 
+}
+
 export interface Group {
   id: number;
   name: string;
+}
+
+export interface Vehicle {
+  vehicleId: number;
+  license_plate_number: string;
+  model_id: string;
+  is_favorite: boolean;
+  vehicle_name: string;
+  image_url: string;
+  model: VehicleModel;
+}
+
+export interface FeatureFlags {
+  subscriptions: boolean;
+}
+
+export interface FilterPreferences {
+  connector_types_filter: any[]; // Update this type as per the actual data structure
+  power_filter: string;
+  include_external_filter: boolean;
+  only_usable_filter: boolean;
+  power_aliases_filter: any[]; // Update this type as per the actual data structure
 }
 
 export interface PrivateInfo {
@@ -19,39 +95,5 @@ export interface PrivateInfo {
   phone_number: string;
   autocharge_enabled: boolean;
   is_marketing_allowed: boolean;
-}
-
-export class User {
-  public id: number;
-  public mail: string;
-  public id_tag: string;
-  public photo: string;
-  public groups: Group[];
-  public status: string;
-  public location: Address;
-  public privateInfo: PrivateInfo;
-
-  constructor(
-    public email: string,
-    public name: string,
-    public address: Address,
-    private _token: string
-  ) {}
-
-  get token() {
-    if (!this.tokenExpirationDate || new Date() > this.tokenExpirationDate) {
-      return null;
-    }
-    return this._token;
-  }
-
-  private get tokenExpirationDate(): Date | undefined {
-    // TODO: Implement logic to retrieve or calculate the token expiration date
-    return undefined; 
-  }
-
-  set token(token: string) {
-    this._token = token;
-  }
-
+  filter_preferences: FilterPreferences;
 }
