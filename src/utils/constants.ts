b79dbe2177dfb6819c15de451547d9874c63fd15
Julio <juliorodriguezramirez@gmail.com>
2023-11-30 13:53:05 +0100

    Privatice consts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,15 +1,15 @@
 import { HttpHeaders } from '@angular/common/http';
 import { environment } from '../environments/environment';
 
-export const BASE_URI_DES = `https://backend-des.wenea.site/api`;
-export const BASE_URI_PRE = `https://backend-pre.wenea.site/api`;
-export const BASE_URI_PRO = `https://backend-pro.wenea.site/api`;
-export const BASE_URI_DEHESA = `https://backend-dehesa.wenea.site/api`;
+const BASE_URI_DES = `https://backend-des.wenea.site/api`;
+const BASE_URI_PRE = `https://backend-pre.wenea.site/api`;
+const BASE_URI_PRO = `https://backend-pro.wenea.site/api`;
+const BASE_URI_DEHESA = `https://backend-dehesa.wenea.site/api`;
 
-export const WENEA_API_VERSION = 'v7';
-export const WENEA_VERSION = environment.version;
-export const BASE_URI: string = BASE_URI_DEHESA;
-export const USER_ENDPOINT = `${BASE_URI}/${WENEA_API_VERSION}/user`;
+const WENEA_API_VERSION = 'v7';
+const WENEA_VERSION = environment.version;
+const BASE_URI: string = BASE_URI_DEHESA;
+const USER_ENDPOINT = `${BASE_URI}/${WENEA_API_VERSION}/user`;
 
 export const WENEA_USER_LOGIN = `${USER_ENDPOINT}/login/`;
 export const WENEA_USER_PROFILE = `${USER_ENDPOINT}/info/`;
