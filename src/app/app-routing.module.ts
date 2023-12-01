1c83e79a5ec839fc6fe8f285773710650698bd52
Julio <juliorodriguezramirez@gmail.com>
2023-12-01 13:26:53 +0100

    Update root routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+	{
+		path: 'login',
+		loadChildren: () => import('./login/login.module').then(m => m.LoginModule),
+	},
 	{
 		path: '',
 		loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule),
 	},
 	{
-		path: 'login',
-		loadChildren: () => import('./login/login.module').then(m => m.LoginModule),
-	},
+    path: 'dashboard',
+    canActivate: [AuthGuard],
+    loadChildren: () =>
+      import('./layout/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      )
+  }
 ];
 
 @NgModule({
