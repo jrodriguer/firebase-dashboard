fa328df8b635e929e4d949dabde54804ca170e23
Julio <juliorodriguezramirez@gmail.com>
2023-12-05 18:17:54 +0100

    Update messaging view

--- src/app/layout/messaging/messaging.component.spec.ts
+++ src/app/layout/messaging/messaging.component.spec.ts
@@ -1,18 +1,28 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MessagingModule } from './messaging.module';
+import { MessagingService } from '../../core/services/messaging.service';
 import { MessagingComponent } from './messaging.component';
+import { PageHeaderComponent } from '../../shared/modules/page-header/page-header.component';
 
 describe('MessagingComponent', () => {
 	let component: MessagingComponent;
 	let fixture: ComponentFixture<MessagingComponent>;
+	let messagingService: jasmine.SpyObj<MessagingService>;
 
 	beforeEach(waitForAsync(() => {
+		const messagingServiceSpy = jasmine.createSpyObj('MessagingService', ['sendMessage']);
+
 		TestBed.configureTestingModule({
-			imports: [MessagingModule, BrowserAnimationsModule, RouterTestingModule],
+			declarations: [MessagingComponent, PageHeaderComponent],
+			imports: [ReactiveFormsModule, BrowserAnimationsModule, RouterTestingModule],
+			providers: [{ provide: MessagingService, useValue: messagingServiceSpy }],
 		}).compileComponents();
+
+		messagingService = TestBed.inject(MessagingService) as jasmine.SpyObj<MessagingService>;
 	}));
 
 	beforeEach(() => {
@@ -23,5 +33,20 @@ describe('MessagingComponent', () => {
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
+	});
+
+	it('should call sendMessage method on form submission', () => {
+		const testTitle = 'Title';
+		const testMessage = 'Message';
+
+		const form = component.messagerForm;
+		form.controls['title'].setValue(testTitle);
+		form.controls['message'].setValue(testMessage);
+
+		messagingService.sendMessage.and.returnValue(of({}));
+
+		component.onSubmit(form);
+
+		expect(messagingService.sendMessage).toHaveBeenCalledWith(testTitle, testMessage);
 	});
 });
--- src/app/layout/messaging/messaging.component.ts
+++ src/app/layout/messaging/messaging.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Subject } from 'rxjs';
+import { MessagingService } from 'src/app/core/services/messaging.service';
 
 @Component({
 	selector: 'app-messaging',
@@ -7,9 +9,19 @@ import { Subject } from 'rxjs';
 	styleUrls: ['./messaging.component.scss'],
 })
 export class MessagingComponent implements OnDestroy {
+	public messagerForm: FormGroup;
 	private destroy$: Subject<void> = new Subject<void>();
 
-	constructor() {}
+	constructor(private messagingSrv: MessagingService) {
+		this.messagerForm = new FormGroup({
+			title: new FormControl(''),
+			message: new FormControl(''),
+		});
+	}
+
+	public onSubmit(form: FormGroup) {
+		this.messagingSrv.sendMessage(form.value.title, form.value.message).subscribe();
+	}
 
 	ngOnDestroy(): void {
 		this.destroy$.next();
