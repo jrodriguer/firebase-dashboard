3093d8f30771ce5721e89f9fa365c4772d450fef
Julio <juliorodriguezramirez@gmail.com>
2023-12-06 23:49:49 +0100

    Error handler test when listVersions called

--- src/app/layout/translations/translations.component.spec.ts
+++ src/app/layout/translations/translations.component.spec.ts
@@ -2,15 +2,20 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of, throwError } from 'rxjs';
 
 import { TranslationsComponent } from './translations.component';
 import { TranslationsModule } from './translations.module';
+import { RemoteConfigService } from '../../core';
+import { VersionInfo } from '../../core/models/remote-config.model';
 
 describe('TranslationsComponent', () => {
 	let component: TranslationsComponent;
 	let fixture: ComponentFixture<TranslationsComponent>;
+	let remoteConfigServiceSpy: jasmine.SpyObj<RemoteConfigService>;
 
 	beforeEach(waitForAsync(() => {
+		const spy = jasmine.createSpyObj('RemoteConfigService', ['listVersions']);
 		TestBed.configureTestingModule({
 			imports: [
 				TranslationsModule,
@@ -18,7 +23,12 @@ describe('TranslationsComponent', () => {
 				RouterTestingModule,
 				HttpClientTestingModule,
 			],
+			providers: [{ provide: RemoteConfigService, useValue: spy }],
 		}).compileComponents();
+
+		remoteConfigServiceSpy = TestBed.inject(
+			RemoteConfigService
+		) as jasmine.SpyObj<RemoteConfigService>;
 	}));
 
 	beforeEach(() => {
@@ -29,5 +39,35 @@ describe('TranslationsComponent', () => {
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
+	});
+
+	it('should handle listVersions success', () => {
+		const version: VersionInfo = {
+			versionNumber: '10',
+			updateOrigin: 'ADMIN_SDK_NODE',
+			updateType: 'INCREMENTAL_UPDATE',
+			updateUser: {
+				name: 'firebase-adminsdk-rx6vt@flutter-news-app-6a808.iam.gserviceaccount.com',
+				email: 'firebase-adminsdk-rx6vt@flutter-news-app-6a808.iam.gserviceaccount.com',
+				imageUrl:
+					'https://lh3.googleusercontent.com/a/ACg8ocLHinnrhpl3Oo1vuRO6E7GWMtcOXfPvYWjVbzOzAsC-=mo',
+			},
+			updateTime: 'Tue, 21 Nov 2023 15:33:45 GMT',
+		};
+		const versions: VersionInfo[] = [version];
+		remoteConfigServiceSpy.listVersions.and.returnValue(of(versions));
+
+		component.getListVersions();
+
+		expect(component.listVersions).toEqual(versions);
+	});
+
+	it('should handle listVersions error', () => {
+		const error = new Error('Error fetching versions');
+		remoteConfigServiceSpy.listVersions.and.returnValue(throwError(error));
+
+		component.getListVersions();
+
+		expect(component.listVersions).toEqual([]);
 	});
 });
