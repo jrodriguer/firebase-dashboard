24868f536a999d5f56ad2f942ba2870c7251dc1b
Julio <juliorodriguezramirez@gmail.com>
2023-12-12 13:34:49 +0100

    Working on validation form

--- src/app/layout/messaging/messaging.component.ts
+++ src/app/layout/messaging/messaging.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Subject } from 'rxjs';
 import { MessagingService } from 'src/app/core/services/messaging.service';
 
@@ -11,20 +11,37 @@ import { MessagingService } from 'src/app/core/services/messaging.service';
 export class MessagingComponent implements OnDestroy {
 	public messagerForm: FormGroup;
 	private destroy$: Subject<void> = new Subject<void>();
+	public submitted: boolean = false;
 
 	constructor(private messagingSrv: MessagingService) {
 		this.messagerForm = new FormGroup({
 			topic: new FormControl(''),
 			token: new FormControl(''),
-			title: new FormControl(''),
-			message: new FormControl(''),
+			title: new FormControl('', [Validators.required]),
+			message: new FormControl('', [Validators.required]),
 		});
 	}
 
+	get title() {
+		return this.messagerForm.get('title');
+	}
+
+	get message() {
+		return this.messagerForm.get('message');
+	}
+
 	public onSubmit(form: FormGroup) {
+		this.submitted = true;
+
+		if (this.messagerForm.invalid) {
+			return;
+		}
+
 		this.messagingSrv
 			.sendMessage(form.value.topic, form.value.token, form.value.title, form.value.message)
 			.subscribe();
+
+		alert('SUCCESS!! \n' + JSON.stringify(this.messagerForm.value, null, 4));
 	}
 
 	ngOnDestroy(): void {
