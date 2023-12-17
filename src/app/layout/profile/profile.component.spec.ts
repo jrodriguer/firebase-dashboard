7f88527cebf168a020b5d96f8093e8898b946d80
Julio <juliorodriguezramirez@gmail.com>
2023-12-17 21:52:50 +0100

    Remove copied version

--- src/app/layout/profile/profile.component.spec.ts
+++ src/app/layout/profile/profile.component.spec.ts
@@ -1,9 +1,7 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-import { RemoteConfigService } from '../../core';
 import { ProfileComponent } from './profile.component';
 import { ProfileModule } from './profile.module';
 
@@ -12,19 +10,8 @@ describe('ProfileComponent', () => {
 	let fixture: ComponentFixture<ProfileComponent>;
 
 	beforeEach(waitForAsync(() => {
-		const spy = jasmine.createSpyObj('RemoteConfigService', [
-			'listVersions',
-			'downloadTemplate',
-			'updateTemplate',
-		]);
 		TestBed.configureTestingModule({
-			imports: [
-				ProfileModule,
-				BrowserAnimationsModule,
-				RouterTestingModule,
-				HttpClientTestingModule,
-			],
-			providers: [{ provide: RemoteConfigService, useValue: spy }],
+			imports: [ProfileModule, BrowserAnimationsModule, RouterTestingModule],
 		}).compileComponents();
 	}));
 
